0209cc6daa1c026742044bedbf7f63ad780c8d78
Abdallah_Abdelsabour <abdallah97158@example.com>
2022-10-04 15:06:54 +0200

    static files

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const http = require("http");
+const path = require('path');
 
-const fs = require("fs");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const reqHandler = require("./routes");
-console.log("start listing ..... ");
+const app = express();
 
-http.createServer(reqHandler.handler).listen(8080);
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
