1792f7bc1a9007fe77bc82b6dcd91d0e638d8632
Abdallah_Abdelsabour <abdallah97158@example.com>
2022-10-04 20:38:55 +0200

    pug template

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.render("404");
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
+const { appendFile } = require("fs");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('shop');
+router.get("/", (req, res, next) => {
+  res.render("shop", { prods: adminData.products, path: "/" });
 });
 
 module.exports = router;
