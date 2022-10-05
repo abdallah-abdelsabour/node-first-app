f5bccbb207d02ac04b1c7af49c4f626f59c169da
Abdallah_Abdelsabour <abdallah97158@example.com>
2022-10-05 09:41:37 +0200

    d

--- routes/admin.js
+++ routes/admin.js
@@ -2,25 +2,17 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+router.get("/add-product", adminController.getAddProduct);
 
-  res.render("add-product");
-});
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  console.log("product arry", products);
-  res.redirect("/");
-});
+router.post("/add-product", adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
